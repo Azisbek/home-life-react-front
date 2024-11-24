5012646c2bcd95e85a4898ed7ba382c83bcfef39
Азисбек <azisbek@MacBook-Air-Azisbek.local>
2024-11-24 22:13:59 +0600

    feature/banner

--- src/widgets/catalog-widget/components/filter-catalog/FilterProduct.jsx
+++ src/widgets/catalog-widget/components/filter-catalog/FilterProduct.jsx
@@ -1,88 +1,88 @@
-import { useRef, useState } from "react";
-import { FilterSelect } from "../../../../components/ui/FilterSelect";
-import s from "./FilterProduct.module.scss";
-import {
-  useGetFilterBrandsQuery,
-  useGetFilterCategoriesQuery,
-  useGetFilterColorQuery,
-} from "../../api/FilterValueCatalogApi";
-import { useDispatch } from "react-redux";
-import { AppButton } from "../../../../components/ui/Button";
-import { setFilter, resetFilters } from "../filter-catalog/model/filter.slice";
+  import { useRef, useState } from "react";
+  import { FilterSelect } from "../../../../components/ui/FilterSelect";
+  import s from "./FilterProduct.module.scss";
+  import {
+    useGetFilterBrandsQuery,
+    useGetFilterCategoriesQuery,
+    useGetFilterColorQuery,
+  } from "../../api/FilterValueCatalogApi";
+  import { useDispatch } from "react-redux";
+  import { AppButton } from "../../../../components/ui/Button";
+  import { setFilter, resetFilters } from "../filter-catalog/model/filter.slice";
 
-export const FilterProduct = () => {
-  const dispatch = useDispatch();
-  const { data: brands } = useGetFilterBrandsQuery();
-  const { data: categories } = useGetFilterCategoriesQuery();
-  const { data: colors } = useGetFilterColorQuery();
+  export const FilterProduct = () => {
+    const dispatch = useDispatch();
+    const { data: brands } = useGetFilterBrandsQuery();
+    const { data: categories } = useGetFilterCategoriesQuery();
+    const { data: colors } = useGetFilterColorQuery();
 
-  const priceMinRef = useRef();
-  const priceMaxRef = useRef();
+    const priceMinRef = useRef();
+    const priceMaxRef = useRef();
 
-  const [filters, setFilters] = useState({
-    category: "",
-    color: "",
-    brand: "",
-  });
+    const [filters, setFilters] = useState({
+      category: "",
+      color: "",
+      brand: "",
+    });
 
-  const handleFilterChange = (filterKey) => (value) => {
-    setFilters((prev) => ({ ...prev, [filterKey]: value }));
+    const handleFilterChange = (filterKey) => (value) => {
+      setFilters((prev) => ({ ...prev, [filterKey]: value }));
+    };
+
+    const handleApplyFilters = () => {
+      const priceMin = priceMinRef.current?.value || "";
+      const priceMax = priceMaxRef.current?.value || "";
+
+      dispatch(setFilter({ key: "category", value: filters.category }));
+      dispatch(setFilter({ key: "color", value: filters.color }));
+      dispatch(setFilter({ key: "brand", value: filters.brand }));
+      dispatch(setFilter({ key: "price_min", value: priceMin }));
+      dispatch(setFilter({ key: "price_max", value: priceMax }));
+    };
+
+    // const handleResetFilters = () => {
+    //   setFilters({
+    //     category: "",
+    //     color: "",
+    //     brand: "",
+    //     priceMin: "",
+    //     priceMax: "",
+    //   });
+    //   dispatch(resetFilters());
+    // };
+
+    return (
+      <div className={s.filterContainer}>
+        <FilterSelect
+          onChange={handleFilterChange("category")}
+          options={categories || []}
+          placeholder='Select Category'
+        />
+        <FilterSelect
+          onChange={handleFilterChange("color")}
+          options={colors || []}
+          placeholder='Select Color'
+        />
+        <FilterSelect
+          onChange={handleFilterChange("brand")}
+          options={brands || []}
+          placeholder='Select Brand'
+        />
+
+        <input
+          placeholder='min'
+          type='number'
+          ref={priceMinRef}
+          name='price_min'
+        />
+        <input
+          placeholder='max'
+          type='number'
+          ref={priceMaxRef}
+          name='price_max'
+        />
+        <AppButton onClick={handleApplyFilters}>Применить</AppButton>
+        <AppButton onClick={() => resetFilters()}>Сбросить</AppButton>
+      </div>
+    );
   };
-
-  const handleApplyFilters = () => {
-    const priceMin = priceMinRef.current?.value || "";
-    const priceMax = priceMaxRef.current?.value || "";
-
-    dispatch(setFilter({ key: "category", value: filters.category }));
-    dispatch(setFilter({ key: "color", value: filters.color }));
-    dispatch(setFilter({ key: "brand", value: filters.brand }));
-    dispatch(setFilter({ key: "price_min", value: priceMin }));
-    dispatch(setFilter({ key: "price_max", value: priceMax }));
-  };
-
-  // const handleResetFilters = () => {
-  //   setFilters({
-  //     category: "",
-  //     color: "",
-  //     brand: "",
-  //     priceMin: "",
-  //     priceMax: "",
-  //   });
-  //   dispatch(resetFilters());
-  // };
-
-  return (
-    <div className={s.filterContainer}>
-      <FilterSelect
-        onChange={handleFilterChange("category")}
-        options={categories || []}
-        placeholder='Select Category'
-      />
-      <FilterSelect
-        onChange={handleFilterChange("color")}
-        options={colors || []}
-        placeholder='Select Color'
-      />
-      <FilterSelect
-        onChange={handleFilterChange("brand")}
-        options={brands || []}
-        placeholder='Select Brand'
-      />
-
-      <input
-        placeholder='min'
-        type='number'
-        ref={priceMinRef}
-        name='price_min'
-      />
-      <input
-        placeholder='max'
-        type='number'
-        ref={priceMaxRef}
-        name='price_max'
-      />
-      <AppButton onClick={handleApplyFilters}>Применить</AppButton>
-      <AppButton onClick={() => resetFilters()}>Сбросить</AppButton>
-    </div>
-  );
-};
