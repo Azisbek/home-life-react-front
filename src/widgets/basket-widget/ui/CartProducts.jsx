f4af280a72748ed478ac3e2da762d77c1dd4c253
DilSan <norms777666@gmail.com>
2025-02-28 16:28:22 +0300

    fixing ui bugs

--- src/widgets/basket-widget/ui/CartProducts.jsx
+++ src/widgets/basket-widget/ui/CartProducts.jsx
@@ -3,10 +3,14 @@ import { CardOrders } from "../../../components/CardOrders/ui/CardOrders";
 import { ProductBasket } from "../../../components/ProductBasket/components/ProductBasket";
 import { Space } from "../../../components/ui/Space/Space";
 import { useGetBasketQuery } from "../api";
+import { TbShoppingCartOff } from "react-icons/tb";
+import { useNavigate } from "react-router-dom";
 import s from "./CartProducts.module.scss";
+import { ROUTE } from "../../../constants/path";
 
 export const CartProducts = () => {
   const { data, refetch } = useGetBasketQuery();
+  const navigate = useNavigate();
 
   const handleQuantityUpdate = () => {
     refetch();
@@ -36,14 +40,25 @@ export const CartProducts = () => {
           </div>
 
           <CardOrders
-            type='navigate'
+            type="navigate"
             quantity={data?.total_quantity}
             subtotal={data?.subtotal}
             totalPrice={data?.totalPrice}
           />
         </div>
       ) : (
-        <h1>В корзине пусто</h1>
+        <div className={s.emptyCartContainer}>
+          <TbShoppingCartOff size={100} className={s.emptyCartImg} />
+
+          <h2>Ваша корзина пуста</h2>
+          <p>Добавьте товары в корзину, чтобы оформить заказ.</p>
+          <button
+            onClick={() => navigate(`${ROUTE.catalog}`)}
+            className={s.btn}
+          >
+            Перейти в каталог
+          </button>
+        </div>
       )}
     </div>
   );
