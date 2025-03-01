c2bb226af1cc6544beb0ccaf3c59d86283ea7b85
Азисбек <azisbek@MacBook-Air-Azisbek.local>
2025-03-01 15:48:16 +0600

    fix

--- src/page/home/ui/HomePage.jsx
+++ src/page/home/ui/HomePage.jsx
@@ -19,6 +19,7 @@ export const HomePage = () => {
           loading={isLoading}
           img={data?.homepage?.banner?.image}
         />
+
         <HomeProductDay
           loading={isLoading}
           data={data?.homepage.product_of_the_day}
